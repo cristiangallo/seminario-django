510410f69ea6de0e003b54540ec74829e821155e
Cristián <mail@cristiangallo.com.ar>
2024-08-17 14:50:37 -0300

    modelos

--- biblioteca/app/models.py
+++ biblioteca/app/models.py
@@ -1,3 +1,23 @@
+# -*- coding: UTF-8 -*-
+
 from django.db import models
 
-# Create your models here.
+
+# app.models.py
+class Autor(models.Model):
+    # Los autores se identifican por un id secuencial y se registra de ellos una descripción
+    # compuesta por su pseudónimo o nombre y apellido.
+
+    # id = models.BigIntegerField(primary_key=True)  <--Django lo hace en forma predeterminada
+    nombre = models.CharField(max_length=30, null=True, blank=True)
+    apellido = models.CharField(max_length=30, null=True, blank=True)
+    pseudonimo = models.CharField(max_length=30, verbose_name='Pseudónimo', null=True, blank=True)
+    modificado = models.DateTimeField(auto_now=True)
+    creado = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Autores'
+        # db_table = 'autores' por defecto app_name_classname en este caso app_autor
+
+    def __str__(self):
+        return self.pseudonimo if self.pseudonimo else "{} {}".format(self.nombre, self.apellido)
